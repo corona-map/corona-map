b8540026dd029f5bda14be577cef2231992594f2
WallE <lamkhanh90@yahoo.com>
2021-08-21 17:37:14 +0200

    Updates

---
+++ precache-manifest.e5c0fd03c866273f3b5f97a35dff3b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39014e3a1f7ba731b5d9aafcece851fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aef95dd28a65a90bf6ec",
+    "url": "/static/css/main.6f019ba5.chunk.css"
+  },
+  {
+    "revision": "74132a0d01b3c3cc9ba4",
+    "url": "/static/js/2.8f816234.chunk.js"
+  },
+  {
+    "revision": "b8a8fb2e10d9dcfe226b5828d3716d5a",
+    "url": "/static/js/2.8f816234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef95dd28a65a90bf6ec",
+    "url": "/static/js/main.a3bbda52.chunk.js"
+  },
+  {
+    "revision": "ded6d55ad132eb36e316",
+    "url": "/static/js/runtime-main.b822f51a.js"
+  }
+]);
